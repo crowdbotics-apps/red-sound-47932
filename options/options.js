db8a7e872657da5f4e9e63ee3096a0d059303562
Crowdbotics <team@crowdbotics.com>
2024-05-14 20:10:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "red-sound-47932",
+  url: "https://red-sound-47932.botics.co",
+  api: "https://red-sound-47932.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
